5eca9dd9b0294d7663612e55f07187d463aa6138
Neitsabes <sebcayet@gmail.com>
2022-10-08 18:33:52 +0200

    Implement level scripts

--- Assets/Scripts/Level/Entity/LevelScript.cs
+++ Assets/Scripts/Level/Entity/LevelScript.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Level", fileName = "Level XX")]
 public class LevelScript : ScriptableObject
 {
-    [SerializeField] private Vector2Int _mapSize;
+    [SerializeField] private int _tileMapId;
+    [SerializeField] private List<Vector2Int> _listSpawnPos;
+    [SerializeField] private List<Robot> _listRobot;
 
-    public Vector2Int mapSize { get { return this._mapSize; } }
+    public int[,] intTileMap { get { return TileMapRepository.instance.GetTileMapById(this._tileMapId); } }
+    public List<LevelRobotSpawn> listRobotSpawn
+    {
+        get {
+            List<LevelRobotSpawn> result = new List<LevelRobotSpawn>();
+            for (int i = 0; i < this._listSpawnPos.Count; i++)
+                result.Add(new LevelRobotSpawn(this._listSpawnPos[i], this._listRobot[i]));
+            return result;
+        }
+    }
 }
