6457d12bfcc0d642775fc9c67ab224dc8e7dff66
Neitsabes <sebcayet@gmail.com>
2022-10-08 11:20:06 +0200

    Created tile map

--- Assets/Scripts/TileMap/TileMapObject.cs
+++ Assets/Scripts/TileMap/TileMapObject.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TileMapObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2Int size { get; protected set; }
+    public GameObject tilePrefab;
+    public TileObject[,] tileMap { get; protected set; }
+
+    public void Start()
     {
-        
+        this.InstantiateTileMap(new Vector2Int(10, 10));
     }
 
-    // Update is called once per frame
-    void Update()
+    public TileObject[,] InstantiateTileMap(Vector2Int mapSize)
     {
-        
+        this.size = mapSize;
+        this.tileMap = new TileObject[mapSize.x, mapSize.y];
+        for (int x = 0; x < mapSize.x; x++)
+            for (int y = 0; y < mapSize.y; y++)
+                this.tileMap[x, y] = TileObject.InstantiateObject(this.tilePrefab, this, new Vector2Int(x, y));
+        return this.tileMap;
     }
 }
