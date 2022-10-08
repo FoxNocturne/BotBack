432905145dbc376ae6b1b0d40af4b87d4410cb06
Lionel Bezamanifary <bezamanifary.lionel@gmail.com>
2022-10-08 16:45:34 +0100

    Robot WIP

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,54 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : BotBackManager
 {
-    public Transform SelectedRobot = null;
-    public GameObject[] Robots;
-    Vector3 pos;
+    public Robot SelectedRobot;
+    public Robot[] Robots;
+    private Vector3 pos = Vector3.zero;
     public float CellSize = 0.2f;              
     public float speed = 1.0f; 
 
+    private void Start()
+    {
+        SelectedRobot = Robots[0];
+    }
     public void GoForward()
     {
-        if ( SelectedRobot )
-        {
-            SelectedRobot.GetComponent<Robot>().GoUp(Vector3.forward * CellSize);
-        }
+        Robots[0].GetComponent<Robot>().GoUp( Vector3.forward);
     }
     public void GoBackward()
     {
-        if (SelectedRobot)
-        {
-            SelectedRobot.GetComponent<Robot>().GoDown(Vector3.back * CellSize);
-        }
+        SelectedRobot.GoDown(pos + Vector3.back * CellSize);
     }
     public void GoLeft()
     {
-        if (SelectedRobot)
-        {
-            SelectedRobot.GetComponent<Robot>().GoLeft(Vector3.left * CellSize);
-        }
+       SelectedRobot.GoLeft(pos + Vector3.left * CellSize);
     }
     public void GoRight()
     {
-        if (SelectedRobot)
-        {
-            SelectedRobot.GetComponent<Robot>().GoRight(Vector3.right * CellSize);
-        }
+        SelectedRobot.GoRight(pos + Vector3.right * CellSize);
     }
 
     void FixedUpdate()
     {
-        if ( SelectedRobot )
-            SelectedRobot.position = Vector3.MoveTowards(SelectedRobot.position, pos, Time.deltaTime * speed);   
+       // if ( SelectedRobot )
+        //    SelectedRobot.position = Vector3.MoveTowards(SelectedRobot.position, pos, Time.deltaTime * speed);   
     }
 
-    public void ChangeBot(  InputAction.CallbackContext context  )  
+    public void ChangeBot(InputAction.CallbackContext context)
     {
-        if ( context.performed &&  Robots.Length >= int.Parse(context.control.name) && Robots[ int.Parse(context.control.name) - 1] )
+        if (context.performed && Robots.Length >= int.Parse(context.control.name) && Robots[int.Parse(context.control.name) - 1])
         {
             Debug.Log(context.control.name);
-            SelectedRobot = Robots[int.Parse(context.control.name) - 1].transform;
-            pos = SelectedRobot.position;
+            //SelectedRobot = Robots[int.Parse(context.control.name) - 1];
+            //pos = SelectedRobot.transform.position;
         }
     }
 
@@ -60,18 +52,8 @@ public class PlayerControler : BotBackManager
     {
         if (SelectedRobot)
         {
-            SelectedRobot.GetComponent<Robot>().Action();
+            //SelectedRobot.GetComponent<Robot>().Action();
             Debug.Log("Action");
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
