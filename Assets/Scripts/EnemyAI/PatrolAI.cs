68bd01b5cb755758de7b482362f3b44b6a1793d1
Hangarter <madison.paz@gmail.com>
2022-10-08 12:28:13 +0200

    Patrolling AI ready

--- Assets/Scripts/EnemyAI/PatrolAI.cs
+++ Assets/Scripts/EnemyAI/PatrolAI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,51 @@ class PatrolAI : EnemyAI
     [SerializeField]
     List<Vector3> patrollingPositions;
 
+    [SerializeField]
+    float goalDistance = 0.1f;
+
+    int currentTargetIndex;
+    Vector3 target;
+
+    private void Start()
+    {
+        currentTargetIndex = 0;
+        target = patrollingPositions[currentTargetIndex];
+
+        StartCoroutine(Patrol());
+    }
+
+    private IEnumerator Patrol()
+    {
+        while(Vector3.Distance(transform.position, target) > goalDistance) {
+            var movement = (target - transform.position).normalized;
+            _movementVector = new Vector2(movement.x, movement.z); // This line moves the enemy!
+            yield return null;
+        }
+
+        currentTargetIndex++;
+        if(currentTargetIndex >= patrollingPositions.Count)
+        {
+            currentTargetIndex = 0;
+        }
+        target = patrollingPositions[currentTargetIndex];
+        
+        StartCoroutine(Patrol());
+    }
+
     protected override void Update() 
     {
-        ExecuteAI();
         base.Update();
     }
 
-    private void ExecuteAI()
+    void OnDrawGizmosSelected()
     {
-        throw new NotImplementedException();
+        // Allows us to visualize where the patrolling points are
+        Gizmos.color = Color.yellow;
+        foreach(var point in patrollingPositions)
+        {
+            Gizmos.DrawCube(point, new Vector3(.1f, .6f, .1f));
+        }
     }
 }
 
