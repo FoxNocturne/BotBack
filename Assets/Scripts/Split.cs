de1093a24bf64b0742c47c04fcc53ae6cf709571
Lionel Bezamanifary <bezamanifary.lionel@gmail.com>
2022-10-08 19:21:45 +0100

    TileMaps and prefab robots

---
+++ Assets/Scripts/Split.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split : BotBackManager, IRobot
+{
+    public enum Stat
+    {
+        up,
+        down,
+        left,
+        right,
+        none
+    }
+
+    public Stat stat;
+    private float size;
+    public Vector3 position { set; get; }
+    public bool isWalking { set; get; }
+    public void GoUp(float size)
+    {
+        stat = Stat.up;
+        this.size = size;
+    }
+    public void GoDown(float size)
+    {
+        stat = Stat.down;
+        this.size = size;
+    }
+    public void GoLeft(float size)
+    {
+        stat = Stat.left;
+        this.size = size;
+    }
+    public void GoRight(float size)
+    {
+        stat = Stat.right;
+        this.size = size;
+    }
+
+    public void Action() { }
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    private void Update()
+    {
+        if (this.isWalking == false)
+        {
+            switch (this.stat)
+            {
+                case Stat.up:
+                    position = position + (Vector3.forward * size);
+                    break;
+                case Stat.down:
+                    position = position + (Vector3.back * size);
+                    break;
+                case Stat.left:
+                    position = position + (Vector3.left * size);
+                    break;
+                case Stat.right:
+                    position = position + (Vector3.right * size);
+                    break;
+            }
+        }
+        if (this.isWalking == true)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, this.position, Time.deltaTime);
+        }
+        if (Vector3.Distance(this.transform.position, this.position) == 0)
+        {
+            this.isWalking = false;
+        }
+        else
+        {
+            this.isWalking = true;
+        }
+
+    }
+
+
+}
