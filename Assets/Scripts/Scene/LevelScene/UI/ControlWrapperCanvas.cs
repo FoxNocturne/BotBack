b745cd01635876d86a622791cb1b85fcd6bbdb2e
quentgamejam <quentgamejam@gmail.com>
2022-10-09 13:51:52 +0200

    g

--- Assets/Scripts/Scene/LevelScene/UI/ControlWrapperCanvas.cs
+++ Assets/Scripts/Scene/LevelScene/UI/ControlWrapperCanvas.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlWrapperCanvas : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    public Image robot;
 
-    public void Show()
+    public void Show(Sprite toshow)
     {
         this._animator.SetBool("isShown", true);
+        robot.sprite = toshow;
     }
 
     public void Hide()
     {
         this._animator.SetBool("isShown", false);
+        robot.sprite = null;
     }
 }
